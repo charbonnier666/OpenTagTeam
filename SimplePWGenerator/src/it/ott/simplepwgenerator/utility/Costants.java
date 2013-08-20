2b2b7540a11cac1cb8e33d057f004ac5406cc1ac
charbonnier666 <riccardo.scarano.lavoro@outlook.com>
2013-08-20 17:30:09 +0200

    Fix

--- SimplePWGenerator/src/it/ott/simplepwgenerator/utility/Costants.java
+++
@@ -1,8 +0,0 @@
-package it.ott.simplepwgenerator.utility;
-
-public final class Costants {
-	public static String LETTERS="abcdefghijklmnopqrstuvwxyz";
-	public static String CAPITALLETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	public static String NUMBERS="0123456789";
-	public static String SPECIALCHARS="~!@#$%^&*()_-+={}[]|:;<>,.?/";
-}
